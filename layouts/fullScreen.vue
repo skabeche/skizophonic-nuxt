36c714c6636826af2b5cf6e78df496b798344f5e
Dan Alcaide <danalc81@gmail.com>
2025-05-01 14:33:42 +0100

    Fix issue with scroll on full screen layout

--- layouts/fullScreen.vue
+++ layouts/fullScreen.vue
@@ -7,9 +7,16 @@
   </div>
 </template>
 
-<style>
-  html {
-    overflow: hidden;
-    overscroll-behavior: none;
-  }
-</style>
+<script setup>
+  onMounted(() => {
+    // Seems to be the only way to disable scroll and pull-to-refresh for this specific layout.
+    document.body.classList.add('overflow-hidden', 'overscroll-none');
+    document.documentElement.classList.add('overflow-hidden', 'overscroll-none');
+  })
+
+  onUnmounted(() => {
+    // Enable scroll and pull-to-refresh again on other layouts.
+    document.body.classList.remove('overflow-hidden', 'overscroll-none');
+    document.documentElement.classList.remove('overflow-hidden', 'overscroll-none');
+  })
+</script>
