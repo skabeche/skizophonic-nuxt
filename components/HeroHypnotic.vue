f7dc2548a8574f0fbbd30991d3d5d5fad41387b5
Dan Alcaide <danalc81@gmail.com>
2025-06-12 00:59:46 +0100

    Add scroll down icon on hero

--- components/HeroHypnotic.vue
+++ components/HeroHypnotic.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hero-hypnotic relative w-screen h-screen">
+  <div ref="heroHypnoticRef" class="hero-hypnotic relative w-screen h-screen">
     <div class="bg-wrapper absolute top-0 left-0 w-full h-full">
       <video ref="videoRef" class="w-full h-full object-cover" preload="auto" autoplay muted loop playsinline disableremoteplayback>
         <source src="/videos/spiral_large.mp4" type="video/mp4" media="(min-width: 2560px)">
@@ -7,18 +7,26 @@
         <source src="/videos/spiral_small.mp4" type="video/mp4">
       </video>
     </div>
-    <h1 ref="headerRef" class="relative z-10 flex justify-center items-center w-full h-full overflow-hidden text-9xl bg-black text-white mix-blend-difference">
+    <h1 ref="headingRef" class="relative z-10 flex justify-center items-center w-full h-full overflow-hidden text-9xl bg-black text-white mix-blend-difference">
       <slot />
     </h1>
+    <div ref="scrollDownRef" class="scroll-down absolute bottom-2 w-full text-4xl sm:text-5xl text-center text-white mix-blend-difference">
+      <Icon class="animate-bounce" name="ion:arrow-down-a" />
+    </div>
   </div>
 </template>
 
 <script setup>
-  import { gsap } from 'gsap'
+  import { gsap } from 'gsap';
+  import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-  const video = useTemplateRef('videoRef')
-  const header = useTemplateRef('headerRef')
-  let ctx
+  gsap.registerPlugin(ScrollTrigger);
+
+  const heroHypnoticRef = useTemplateRef('heroHypnoticRef');
+  const videoRef = useTemplateRef('videoRef');
+  const headingRef = useTemplateRef('headingRef');
+  const scrollDownRef = useTemplateRef('scrollDownRef');
+  let ctx;
 
   onMounted(() => {
     ctx = gsap.context(() => {
@@ -27,34 +35,47 @@
           defaults: {
             delay: .4
           },
+          onStart: () => {
+            gsap.set(scrollDownRef.value, {
+              autoAlpha: 0,
+              filter: 'blur(3px)',
+            })
+          }
         })
-        .fromTo(video.value, {
+        .fromTo(videoRef.value, {
           clipPath: 'circle(0% at 50% 50%)',
         }, {
           duration: 2,
           clipPath: 'circle(100% at 50% 50%)',
         })
-        .fromTo(header.value, {
+        .fromTo(headingRef.value, {
           autoAlpha: 0,
           scale: 1.1,
           filter: 'blur(5px)',
         }, {
-          autoAlpha: 1,
           duration: .5,
+          autoAlpha: 1,
           scale: 1,
           filter: 'blur(0px)',
         }, '<')
+        .to(scrollDownRef.value, {
+          duration: .3,
+          autoAlpha: 1,
+          filter: 'blur(0px)',
+        }, '<0.3')
 
-      // gsap.to(video.value, {
-      //   filter: 'blur(6px)',
-      //   duration: 1,
-      //   repeat: -1,
-      //   yoyo: true,
-      //   ease: 'sine.inOut'
-      // });
-
-    }); // <- Scope!
-
+      gsap.to(scrollDownRef.value, {
+        duration: .3,
+        autoAlpha: 0,
+        filter: 'blur(3px)',
+        scrollTrigger: {
+          trigger: heroHypnoticRef.value,
+          start: '5% top',
+          // markers: true,
+          toggleActions: 'play none none reverse',
+        }
+      })
+    }, heroHypnoticRef.value); // <- Scope!
   })
 
   onUnmounted(() => {
