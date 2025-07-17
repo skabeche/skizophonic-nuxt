28599646ccf2570c5fe2fbac584fae361c3bf230
Dan Alcaide <danalc81@gmail.com>
2025-07-17 12:40:23 +0100

    Optimize images on band page

--- pages/band.vue
+++ pages/band.vue
@@ -15,7 +15,7 @@
     </div>
     <figure v-for="(image, index) in images" :key="image.src" :class="`figure-${index}`" class="relative flex-auto overflow-hidden">
       <div class="relative">
-        <NuxtImg class="max-lg:h-auto h-[50dvh] w-full object-cover transition-all duration-500 ease-in-out cursor-pointer will-change-transform lg:scale-105 hover:scale-110" width="1920" loading="lazy" :alt="image.alt" :src="`/images/band/${image.src}`" @click="handleClick" @mousemove="handleMouseMove" />
+        <NuxtImg class="max-lg:h-auto h-[50dvh] w-full object-cover transition-all duration-500 ease-in-out cursor-pointer will-change-transform lg:scale-105 hover:scale-110" sizes="400px md:640px xl:1920px" :alt="image.alt" :src="`/images/band/${image.src}`" @click="handleClick" @mousemove="handleMouseMove" />
       </div>
       <figcaption class="relative z-10 text-white bg-black">{{ image.alt }}</figcaption>
     </figure>
