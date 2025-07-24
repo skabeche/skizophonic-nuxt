e000469ec3c81b4b6c1b39957fc78e850eb086c9
Dan Alcaide <danalc81@gmail.com>
2025-07-24 01:09:24 +0100

    Minor changes on footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,13 +1,14 @@
 <template>
   <footer class="bg-black">
-    <div class="bg-white mt-40 rounded-b-3xl">
-      <AppLogo class="fill-black mx-auto lg:mx-0 w-3/4 xl:w-1/2" />
+    <div class="bg-white mt-40 lg:mt-50 rounded-b-3xl">
+      <AppLogo class="relative z-20 fill-black mx-auto lg:mx-0 w-3/4 xl:w-1/2" />
     </div>
-    <div class="relative z-20 bg-black h-[250px] [clip-path:polygon(0_0,_100%_0,_100%_100%,_0%_100%)]">
+    <div class="relative z-20 bg-black h-[250px] [clip-path:inset(0_0_0_0)]">
       <div class="fixed left-0 bottom-0 h-[250px] bg-black w-full flex flex-col items-center lg:items-start text-center lg:text-left gap-6 p-14 md:py-40 text-white">
         <SocialLinks class="text-4xl" />
-        <div class="copyright [&_a]:anim-underline-white">
-          <p>&copy;{{ currentYear }} Skizophonic &infin; {{ $t('footer.siteBy') }} <a href="https://www.danalcaide.com">danalcaide.com</a></p>
+        <div class="copyright flex flex-col lg:flex-row gap-2 [&_a]:anim-underline-white">
+          <p>&copy; {{ currentYear }} Skizophonic</p>
+          <p>&infin; {{ $t('footer.siteBy') }} <a href="https://www.danalcaide.com">danalcaide.com</a></p>
         </div>
       </div>
     </div>
