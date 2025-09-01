5217606a4bbe9a40ec553408a263479de1bc9cc6
Dan Alcaide <danalc81@gmail.com>
2025-09-01 16:51:30 +0100

    Enable SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false, // Need to disable server-side rendering to prevent error 500 (no idea why yet).
+  // ssr: false, // Need to disable server-side rendering to prevent error 500 (no idea why yet).
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
