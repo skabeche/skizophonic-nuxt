b323bb73190e74c61727bc8777b74f6aacd27bb3
Dan Alcaide <danalc81@gmail.com>
2025-09-06 21:36:25 +0100

    Update translation

--- app/components/blocks/AlbumArtwork.vue
+++ app/components/blocks/AlbumArtwork.vue
@@ -22,7 +22,7 @@
     </section>
   </AnimationReveal>
   <AnimationReveal class="hidden touch:block hybrid-device:block">
-    <div ref="moreInfoRef" class="more-info w-full left-0 bottom-0 text-xs pt-2 lowercase">[{{ t('components.albumArtwork.moreInfo') }}]</div>
+    <div ref="moreInfoRef" class="more-info w-full left-0 bottom-0 text-xs pt-2">[{{ t('components.albumArtwork.moreInfo') }}]</div>
   </AnimationReveal>
 </template>
 
